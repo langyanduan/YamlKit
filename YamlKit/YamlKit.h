dbfc494f6d4c9960db94dae034a268fa0b05e6a0
langyanduan <langyanduan@qq.com>
2017-05-21 13:27:44 +0800

    Initial Commit

---
+++ YamlKit/YamlKit.h
@@ -0,0 +1,19 @@
+//
+//  YamlKit.h
+//  YamlKit
+//
+//  Created by wufan on 2017/5/21.
+//  Copyright © 2017年 atoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YamlKit.
+FOUNDATION_EXPORT double YamlKitVersionNumber;
+
+//! Project version string for YamlKit.
+FOUNDATION_EXPORT const unsigned char YamlKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YamlKit/PublicHeader.h>
+
+
